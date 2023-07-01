b9310c3be2fd4328e9dce8035d4fb892df392e8e
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-07-01 15:30:31 +0300

    Ticketing app data is completed

--- src/main/java/com/company/controller/TaskController.java
+++ src/main/java/com/company/controller/TaskController.java
@@ -76,7 +76,7 @@ public class TaskController {
     }
 
     @PostMapping("/update/{id}")
-    public String updateTask(@ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
 
